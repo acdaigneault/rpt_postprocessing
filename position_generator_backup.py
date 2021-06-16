f335e2d9bcc3055e6016e25224e4aa2bacf34930
acdaigneault <71884806+acdaigneault@users.noreply.github.com>
2021-06-16 16:58:04 -0400

    [wip]

--- position_generator_backup.py
+++ position_generator_backup.py
@@ -205,60 +205,83 @@ ax.set_ylabel("y")
 ax.set_zlabel("z")
 
 
+
+### Set 3 : Constant angles & z ###
+
+theta = np.pi/4
+phi = 0
+
+# Max distance for the angle
+dmax = 2*R*np.cos(theta)/np.cos(phi)
+
+# Get the unit vector
+ptmax = np.array([dmax*np.cos(phi)*np.cos(theta) - FP[0], # Because detector x orientation has opposite sign
+         dmax*np.cos(phi)*np.sin(theta) + FP[1],
+         dmax*np.sin(phi) + FP[2]])
+
+vector = ptmax - np.array(FP) # Vector of the detector fp to point at max distance
+
+f_max = np.linalg.norm(vector) # Norm = max factor of the unit vector for the distance
+unit_vector = vector/f_max
+
+# Generate position at contant angles
+f_vector = np.linspace(0, f_max, nb)
+positions = np.ones((nb,3))
+distance3 = np.ones(nb)
+for i in range(0,nb):
+    positions[i] = f_vector[i] * unit_vector + np.array(FP) # factor * unit vector + face position detector
+    distance3[i] = np.linalg.norm(f_vector[i] * unit_vector)
+
+x3 = positions[:,0]
+y3 = positions[:,1]
+z3 = positions[:,2]
+ax1.plot(x3, y3, z3,  ".", color="red", label="angles & z = constant")
+
+
+### Set 4 : Constant angles  ###
+
+theta = np.pi/4
+phi = theta
+
+# Max distance for the angle
+dmax = 2*R*np.cos(theta)/np.cos(phi)
+
+# Get the unit vector
+ptmax = np.array([dmax*np.cos(phi)*np.cos(theta) - FP[0], # Because detector x orientation has opposite sign
+         dmax*np.cos(phi)*np.sin(theta) + FP[1],
+         dmax*np.sin(phi) + FP[2]])
+
+vector = ptmax - np.array(FP) # Vector of the detector fp to point at max distance
+
+f_max = np.linalg.norm(vector) # Norm = max factor of the unit vector for the distance
+unit_vector = vector/f_max
+
+# Generate position at contant angles
+f_vector = np.linspace(0, f_max, nb)
+positions = np.ones((nb,3))
+distance4 = np.ones(nb)
+for i in range(0,nb):
+    positions[i] = f_vector[i] * unit_vector + np.array(FP) # factor * unit vector + face position detector
+    distance4[i] = np.linalg.norm(f_vector[i] * unit_vector)
+
+x4 = positions[:,0]
+y4 = positions[:,1]
+z4 = positions[:,2]
+ax1.plot(x4, y4, z4,  ".", color="blue", label="angles = constant")
+
+
 ### Set 9 : Constant angles  ###
 
 theta = -np.pi/3
 phi = -np.pi/4
 
-bigR = FP[0] # Radius of center of reactor to face position of detector
-
-# Max distance for the angle (from FP to opposite side of reactor)
+# Max distance for the angle
 dmax = 2*R*np.cos(theta)/np.cos(phi)
 
-xmax = dmax*np.cos(phi)*np.cos(theta) - (FP[0] - (FP[0] - R))  # Because detector x orientation has opposite sign
-ymax = dmax*np.cos(phi)*np.sin(theta) + FP[1]
-zmax = dmax*np.sin(phi) + FP[2]
-
-dmax = np.linalg.norm([xmax, ymax, zmax])
-
-
-if zmax > L:
-    zmax = L
-elif zmax < 0:
-    zmax = 0
-
-
-dmax = (zmax - FP[2])/np.sin(phi)
-xmax = dmax*np.cos(phi)*np.cos(theta) - (FP[0] - (FP[0] - R))
-ymax = dmax * np.cos(phi) * np.sin(theta) + FP[1]
-
-ax1.plot(xmax, ymax, zmax, '.', color="red")
-
-print('phi : ', phi, np.arcsin((zmax - FP[2])/(dmax)))
-
-"""
-dmax0 = 0
-while dmax0 != dmax:
-
-    if xmax > (bigR+R)*np.cos(theta)*np.cos(theta) - FP[0]:
-        xmax = (bigR+R)*np.cos(theta)**2 - FP[0]
-        dmax = (xmax + FP[0])/np.cos(phi)*np.cos(theta)
-
-    if ymax > (bigR+R)*np.cos(theta)*np.sin(theta) + FP[1]:
-        ymax = (bigR+R)*np.cos(theta)*np.sin(theta) + FP[1]
-        dmax = (ymax - FP[1]) / np.cos(phi) * np.sin(theta)
-
-    zmax = dmax * np.sin(phi) + FP[2]
-
-    dmax0 = dmax
-
-"""
-
-a = (xmax-FP[0])/(ymax-FP[1])
-xmin = np.sqrt()
-
 # Get the unit vector
-ptmax = np.array([xmax, ymax, zmax])
+ptmax = np.array([dmax*np.cos(phi)*np.cos(theta) - FP[0], # Because detector x orientation has opposite sign
+         dmax*np.cos(phi)*np.sin(theta) + FP[1],
+         dmax*np.sin(phi) + FP[2]])
 
 vector = ptmax - np.array(FP) # Vector of the detector fp to point at max distance
 
@@ -301,12 +324,41 @@ ax1.set_zlabel("z")
 
 y5 = FP[1]*np.ones(nb)
 z5 = FP[2]*np.ones(nb)
-x5 = np.linspace(-R, R, nb)
+x5 = np.linspace(FP[0], FP[0] - 2*R, nb)
 
 distance5 = x5
 
 ax2.plot(x5, y5, z5,  ".", color="black")
 
+### Set 6 : Case S3, y & z = cte  ###
+
+y6 = FP[1]*np.ones(nb)
+z6 = FP[2]*np.ones(nb) + R
+x6 = np.linspace(FP[0], FP[0] - 2*R, nb)
+
+distance6 = x6
+
+ax2.plot(x6, y6, z6,  ".", color="black")
+
+### Set 7 : x & z = cte  ###
+
+y7 = np.linspace(-R, R, nb)
+z7 = FP[2]*np.ones(nb)
+x7 = 0*np.ones(nb)
+
+distance7 = y7
+
+ax2.plot(x7, y7, z7,  ".", color="black")
+
+### Set 8 : x & y = cte  ###
+
+x8 = 0*np.ones(nb)
+y8 = FP[1]*np.ones(nb)
+z8 = np.linspace(0, L, nb)
+
+distance8 = z8
+
+ax2.plot(x8, y8, z8,  ".", color="black")
 
 reactor = ax2.plot_surface(Xc, Yc, Zc, alpha=0.3, color="grey", label="Reactor")
 detector = ax2.plot_surface(Xd+FP[0], Yd, Zd, alpha=0.3, color="blue", label="Detector")
@@ -324,11 +376,11 @@ plt.show()
 
 
 ### Export positions ###
-#data = pd.DataFrame(columns=["x_position", "y_position", "z_position", "angle_distance"])
-#data["x_position"] = np.concatenate((x0, x1, x2, x3, x4, x9, x5, x6, x7, x8), axis=None)
-#data["y_position"] = np.concatenate((y0, y1, y2, y3, y4, y9, y5, y6, y7, y8), axis=None)
-#data["z_position"] = np.concatenate((z0, z1, z2, z3, z4, z9, z5, z6, z7, z8), axis=None)
-#data["angle_distance"] = np.concatenate((alpha0, alpha1, alpha2, distance3, distance4, distance9, distance5, distance6, distance7, distance8), axis=None)
+data = pd.DataFrame(columns=["x_position", "y_position", "z_position", "angle_distance"])
+data["x_position"] = np.concatenate((x0, x1, x2, x3, x4, x9, x5, x6, x7, x8), axis=None)
+data["y_position"] = np.concatenate((y0, y1, y2, y3, y4, y9, y5, y6, y7, y8), axis=None)
+data["z_position"] = np.concatenate((z0, z1, z2, z3, z4, z9, z5, z6, z7, z8), axis=None)
+data["angle_distance"] = np.concatenate((alpha0, alpha1, alpha2, distance3, distance4, distance9, distance5, distance6, distance7, distance8), axis=None)
 
 
 #path_export_data = "/mnt/DATA/rpt_postprocessing/positions"
